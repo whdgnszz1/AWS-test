550ba4ce628849d469c62fca7bbb1a1a5e0d2ab3
whdgnszz1 <whdgnszz1@snu.ac.kr>
2023-08-10 14:36:07 +0900

    38

--- scripts/setenv.sh
+++ scripts/setenv.sh
@@ -2,13 +2,13 @@
 
 #!/bin/bash
 
-echo "export MONGO_USER=${MONGO_USER}" >> ~/.bashrc
-echo "export MONGO_PASS=${MONGO_PASS}" >> ~/.bashrc
-echo "export SECRET_KEY=${SECRET_KEY}" >> ~/.bashrc
-echo "export FLASK_SECRET_KEY=${FLASK_SECRET_KEY}" >> ~/.bashrc
-echo "export GOOGLE_CLIENT_ID=${GOOGLE_CLIENT_ID}" >> ~/.bashrc
-echo "export GOOGLE_CLIENT_SECRET=${GOOGLE_CLIENT_SECRET}" >> ~/.bashrc
-echo "export KAKAO_CLIENT_ID=${KAKAO_CLIENT_ID}" >> ~/.bashrc
+ENV_FILE="/home/ubuntu/jonghun/.env"
 
-# 환경 변수 적용
-source ~/.bashrc
+# .env 파일에 환경변수 저장
+echo "MONGO_USER=${MONGO_USER}" > $ENV_FILE
+echo "MONGO_PASS=${MONGO_PASS}" >> $ENV_FILE
+echo "SECRET_KEY=${SECRET_KEY}" >> $ENV_FILE
+echo "FLASK_SECRET_KEY=${FLASK_SECRET_KEY}" >> $ENV_FILE
+echo "GOOGLE_CLIENT_ID=${GOOGLE_CLIENT_ID}" >> $ENV_FILE
+echo "GOOGLE_CLIENT_SECRET=${GOOGLE_CLIENT_SECRET}" >> $ENV_FILE
+echo "KAKAO_CLIENT_ID=${KAKAO_CLIENT_ID}" >> $ENV_FILE
