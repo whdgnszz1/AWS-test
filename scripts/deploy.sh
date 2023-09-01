e6dd8c413f766add603ab86ae3b2974d5953601d
whdgnszz1 <whdgnszz1@snu.ac.kr>
2023-09-01 11:31:49 +0900

    1

---
+++ scripts/deploy.sh
@@ -0,0 +1,27 @@
+REPOSITORY=/home/ubuntu/node_level4-5_express
+NODE_APP_DIR=/home/ubuntu/node_level4-5_express
+ENV_PATH=$NODE_APP_DIR/.env
+cd $REPOSITORY
+
+NODE_PID=$(pgrep -f "node")
+if [ -z $NODE_PID ]
+then
+  echo "> 종료할 Node Express 애플리케이션이 없습니다."
+else
+  echo "> kill Node Express app with PID: $NODE_PID"
+  kill -15 $NODE_PID
+  sleep 5
+fi
+
+if [ -f $ENV_PATH ]; then
+    source $ENV_PATH
+fi
+
+echo "> Installing dependencies"
+cd $NODE_APP_DIR
+yarn install
+
+# Node Express 앱 시작
+echo "> Starting Node Express app"
+cd $NODE_APP_DIR
+yarn start
